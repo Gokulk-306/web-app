57639fc5b8c3bb3b721670f18a70010c76fbf326
Gokulk-306 <ceecgokul2024@gmail.com>
2025-10-15 22:12:22 +0530

    feat: Server.js Edited

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Jenkins server + Docker Web App 🚀</h1>');
+  res.send('<h1>Hello from Jenkins + Docker Web App 🚀</h1>');
 });
 
 const PORT = process.env.PORT || 3000;
